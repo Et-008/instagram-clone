e193b885e2a46b6909a0f498743b372ca062bb38
Et-008 <61292452+Et-008@users.noreply.github.com>
2020-11-15 22:47:44 +0530

    Display changes made

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,21 +3,23 @@ import "./Auth.css";
 
 let authentication = (props) => {
   return (
-    <div className="Login_Signup">
+    <div className="Login_Signup_Container">
       Login to Continue
       <form className="Login-Form">
-        <label>
-          Name :{" "}
+        <label className="Input_Label">
+          Name:
           <input
+            className="Input_Areas"
             value={props.Name}
             onChange={() => props.Namechanged()}
             type="text"
             required
           ></input>
         </label>
-        <label>
-          Password :{" "}
+        <label className="Input_Label">
+          Passwordsvsdvs:
           <input
+            className="Input_Areas"
             value={props.Password}
             onChange={() => props.PasswordChanged()}
             type="password"
