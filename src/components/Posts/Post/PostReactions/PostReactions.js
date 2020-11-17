0862e621b8ccb3421f0aad966c40ac46485dcae8
Et-008 <61292452+Et-008@users.noreply.github.com>
2020-11-17 20:42:21 +0530

    Like Reaction V1 added

--- src/components/Posts/Post/PostReactions/PostReactions.js
+++ src/components/Posts/Post/PostReactions/PostReactions.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Icons from "../../../../assets/Icons/Icons";
 import "./PostReactions.css";
 
 let PostReaction = (props) => {
+  let [HeartClasses, setHeartClasses] = useState("ReactionIconsHeart");
+  let Like = () => {
+    if (HeartClasses == "ReactionIconsHeart") {
+      setHeartClasses("Liked");
+    }
+    if (HeartClasses == "Liked") {
+      setHeartClasses("ReactionIconsHeart");
+    }
+  };
   return (
     <div className="PostReactions">
       <div className="ReactionIconsDiv">
-        <img className="Icon ReactionIcons" alt="Heart" src={Icons.Heart} />
+        <div id="heart" onClick={() => Like()} className={HeartClasses}></div>
         <img
           className="Icon ReactionIcons"
           alt="Comments"
--- src/components/UploadPost/UploadPost.js
+++ src/components/UploadPost/UploadPost.js
@@ -1,25 +1,46 @@
-import React, { useState } from 'react';
-import './UploadPost.css';
+import React, { useState } from "react";
+import "./UploadPost.css";
 
 let UploadPost = () => {
-    let [Img, setImg] = useState(<img className="UploadPreview" src={"https://www.transparenttextures.com/patterns/brushed-alum.png"} />);
-    let Upload = (event) => {
-        let SelectedImages = event.target.files;
-        let ArrayofImages = [];
-        console.log(SelectedImages);
-        for (var i=0; i<SelectedImages.length; i++) {
-            ArrayofImages.push(<img className="UploadPreview" src={URL.createObjectURL(SelectedImages[i])} />)
-        };
-        setImg(ArrayofImages);
+  let [Img, setImg] = useState([]);
+  let Upload = (event) => {
+    let SelectedImages = event.target.files;
+    let ArrayofImages = [];
+    for (var i = 0; i < SelectedImages.length; i++) {
+      ArrayofImages.push(
+        <img
+          className="UploadPreview"
+          alt="https://www.transparenttextures.com/patterns/brushed-alum.png"
+          src={URL.createObjectURL(SelectedImages[i])}
+        />
+      );
     }
-    return (
+    if (ArrayofImages.length) {
+      setImg(ArrayofImages);
+    }
+  };
+  return (
     <div className="Upload">
-        {Img}
-        <label for="file-upload" className="UploadInput">Select Upload Files
-            <input id="file-upload" type="file" multiple onChange={Upload}></input>
+      {Img.length ? (
+        Img
+      ) : (
+        <label for="file-upload" className="UploadSelect">
+          <input
+            id="file-upload"
+            type="file"
+            multiple
+            onChange={Upload}
+          ></input>
         </label>
+      )}
+
+      <label className="UploadInput">
+        Title
+        <input type="text"></input>
+      </label>
+      <button className="UploadInput">Post</button>
     </div>
-    )
-}
+  );
+};
 
 export default UploadPost;
